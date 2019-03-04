b177ab8067d1a17f0a89c31d707432624ba0e3ff
NievesBorrero <nievesborrerobarea@gmail.com>
2019-03-04 14:25:54 +0100

    Add tests to spreadsheet reader service

--- tests/test_spreadsheet_reader.py
+++ tests/test_spreadsheet_reader.py
@@ -22,3 +22,19 @@ class SpreadsheetReaderTestCase(unittest.TestCase):
         values_sheet = self.service.get_values_from_sheet()
 
         self.assertEqual(len(values_sheet), 1)
+
+    @patch.object(SpreadsheetReader, 'get_first_sheet')
+    def test_get_first_sheet(self, get_first_sheet):
+        get_first_sheet.return_value = 'sheet 1'
+
+        result = self.service.get_first_sheet('1')
+
+        self.assertEqual(result, 'sheet 1')
+
+    @patch.object(SpreadsheetReader, 'open_sheet')
+    def test_open_sheet(self, open_sheet):
+        open_sheet.return_value = 'sheet'
+
+        result = self.service.open_sheet('1')
+
+        self.assertEqual(result, 'sheet')
