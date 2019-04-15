26b2ea02ec02caaf91a4761244cd28eadf77f2d7
Javier Aguirre <contacto@javaguirre.net>
2019-04-15 17:02:54 +0200

    Added Flask server and deps

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask, request
+
+from main import syncronize_intents
+
+app = Flask(__name__)
+
+
+@app.route("/sync")
+def sync():
+    token = request.args.get('token')
+
+    if not token or os.environ.get('TOKEN') != token:
+        return
+
+    syncronize_intents()
+    return "Sincronización completada con éxito"
