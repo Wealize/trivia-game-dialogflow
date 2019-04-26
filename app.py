f8ade8ee1144ee7c5855f1e81d4049a6976a8833
Javier Aguirre <contacto@javaguirre.net>
2019-04-26 22:28:36 +0200

    Finish tuning for Heroku deployment

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 from main import syncronize_intents
 
@@ -16,3 +16,50 @@ def sync():
 
     syncronize_intents()
     return "Sincronización completada con éxito"
+
+
+@app.route("/hook", methods=['POST'])
+def webhook():
+    token = request.args.get('token')
+
+    if not token or os.environ.get('TOKEN') != token:
+        return 'Forbidden'
+
+    request_dialogflow = request.get_json()
+    session_id = request_dialogflow['session'].split('/')[-1]
+    print(request_dialogflow)
+    print(session_id)
+    return jsonify(
+        {
+            "fulfillmentText": "Holiiii",
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": ["Mi respuestica"]
+                    }
+                }
+            ],
+            "source": "vocento.com",
+            "payload": {
+                "google": {
+                    "expectUserResponse": True,
+                    "richResponse": {
+                        "items": [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": "Esto es una respuesta simple"
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            "outputContexts": [
+                {
+                    "name": "projects/vocento-staging/agent/sessions/{}/contexts/pliki".format(session_id),
+                    "lifespanCount": 5,
+                    "parameters": {}
+                }
+            ]
+        }
+    )
