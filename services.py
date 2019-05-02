84ab817a5ab00368e0ef3c1ea4c0863579c5c61d
Javier Aguirre <contacto@javaguirre.net>
2019-05-02 17:01:43 +0200

    moving logic to service

--- services.py
+++ services.py
@@ -4,9 +4,7 @@ import random
 
 import gspread
 import redis
-import dialogflow
 from oauth2client.service_account import ServiceAccountCredentials
-from google.oauth2 import service_account
 from slugify import slugify
 
 
@@ -27,11 +25,77 @@ class PersistService:
 
 
 class QuestionService:
-    def __init__(self, questions):
+    CONTEXT_PATTERN = 'projects/{}/agent/sessions/{}/contexts/{}'
+    CORRECT_QUESTION_FORMAT = 'Correcto! , {} {} ¿Quieres volver a jugar?'
+    INCORRECT_QUESTION_FORMAT = 'Incorrecto, la respuesta correcta es: {}, ¿Quieres volver a jugar?'
+
+    def __init__(self, project_id, session_id, questions):
+        self.project_id = project_id
+        self.session_id = session_id
         self.questions = questions
 
     def get_question(self):
         return random.choice(self.questions)
+
+    def get_context(self, name):
+        return self.CONTEXT_PATTERN.format(
+            self.project_id,
+            self.session_id,
+            name
+        )
+
+    def get_question_from_context(self, contexts, new_context):
+        question_object = None
+
+        for context in contexts:
+            if context['name'] == new_context:
+                question_context = context['parameters']['question']
+
+                for question in self.questions:
+                    if question['context'] == question_context:
+                        question_object = question
+
+        return question_object
+
+    def get_response_to_question(self, text, question_object):
+        if text in question_object['correct_response']:
+            response = self.CORRECT_QUESTION_FORMAT.format(
+                question_object['correct_response'],
+                question_object['description']
+            )
+        else:
+            response = self.INCORRECT_QUESTION_FORMAT.format(
+                question_object['correct_response'])
+
+        return response
+
+    def get_dialogflow_response(self, response, question, contexts):
+        return {
+            "fulfillmentText": response,
+            "fulfillmentMessages": [
+                {
+                    "text": {
+                        "text": [response]
+                    }
+                }
+            ],
+            "source": "abc.theneonproject.org",
+            "payload": {
+                "google": {
+                    "expectUserResponse": True,
+                    "richResponse": {
+                        "items": [
+                            {
+                                "simpleResponse": {
+                                    "textToSpeech": question['text']
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            "outputContexts": contexts
+        }
 
 
 class SpreadsheetReader:
@@ -56,4 +120,12 @@ class SpreadsheetReader:
     def get_values_from_sheet(self):
         values_list = self.worksheet.get_all_values()
         values_list.pop(0)
-        return values_list
+        return [
+            {
+                'text': question[0],
+                'context': slugify(question[0], to_lower=True),
+                'correct_response': question[1],
+                'description': question[2]
+            }
+            for question in values_list
+        ]
