0e2611efe3e79086c81cd3feaca53f894eb4703d
Javier Aguirre <contacto@javaguirre.net>
2019-05-02 17:01:33 +0200

    small refactor, moving question service to services

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import json
 from flask import Flask, request, jsonify, g
 from slugify import slugify
 
-from services import (QuestionService, PersistService,
-                      SpreadsheetReader, IntentsSyncronizer)
+from services import QuestionService, PersistService, SpreadsheetReader
 from middleware import check_authentication
 
 
 INTENT_PARENT = 'game'
 PROJECT_ID = os.environ.get('PROJECT_ID')
+SPREADSHEET_ID = os.environ.get('SPREADSHEET_ID')
+CREDENTIALS = json.loads(os.environ.get('SPREADSHEET_CREDENTIALS'))
 app = Flask(__name__)
 persist_service = PersistService(os.environ.get('REDIS_URL'))
 
@@ -18,21 +19,9 @@ persist_service = PersistService(os.environ.get('REDIS_URL'))
 @app.route("/sync")
 @check_authentication
 def sync():
-    spreadsheet_id = os.environ.get('SPREADSHEET_ID')
-    credentials = json.loads(os.environ.get('SPREADSHEET_CREDENTIALS'))
-
-    reader = SpreadsheetReader(spreadsheet_id, credentials)
-    intents = reader.get_values_from_sheet()
-    questions = [
-        {'text': intent[0],
-         'context': slugify(intent[0], to_lower=True),
-         'correct_response': intent[1],
-         'description': intent[2]
-         }
-        for intent in intents
-    ]
+    reader = SpreadsheetReader(SPREADSHEET_ID, CREDENTIALS)
+    questions = reader.get_values_from_sheet()
     persist_service.set_questions(questions)
-
     return "Sincronización completada con éxito"
 
 
@@ -42,29 +31,22 @@ def webhook():
     request_dialogflow = request.get_json()
     print(request_dialogflow)
     session_id = request_dialogflow['session'].split('/')[-1]
-    project_id = PROJECT_ID
     request_text = request_dialogflow['queryResult']['queryText']
-
     questions = persist_service.get_questions()
-    question = QuestionService(questions).get_question()
-    new_context = "projects/{}/agent/sessions/{}/contexts/{}".format(
-        project_id, session_id, 'question')
+    question_service = QuestionService(PROJECT_ID, session_id, questions)
 
-    contexts = request_dialogflow['queryResult']['outputContexts']
-    question_object = None
-
-    for context in contexts:
-        if context['name'] == new_context:
-            question_context = context['parameters']['question']
-
-            for question in questions:
-                if question['context'] == question_context:
-                    question_object = question
+    # TODO Save questions already answered on session
+    question = question_service.get_question()
+    new_context = question_service.get_context('question')
+    question_object = question_service.get_question_from_context(
+        request_dialogflow['queryResult']['outputContexts'],
+        new_context
+    )
 
     if question_object:
         new_context = "projects/{}/agent/sessions/{}/contexts/{}".format(
-            project_id, session_id, 'game')
-        outputContexts = [
+            PROJECT_ID, session_id, 'game')
+        output_contexts = [
             {
                 "name": new_context,
                 "lifespanCount": 1,
@@ -72,15 +54,11 @@ def webhook():
             }
         ]
 
-        if request_text in question_object['correct_response']:
-            response = ' '.join(
-                [question_object['correct_response'], question_object['description']])
-        else:
-            response = 'Incorrecto, la respuesta correcta es: {}'.format(
-                question_object['correct_response'])
+        response = question_service.get_response_to_question(
+            request_text, question_object)
     else:
         response = question['text']
-        outputContexts = [
+        output_contexts = [
             {
                 "name": new_context,
                 "lifespanCount": 1,
@@ -91,30 +69,6 @@ def webhook():
         ]
 
     return jsonify(
-        {
-            "fulfillmentText": response,
-            "fulfillmentMessages": [
-                {
-                    "text": {
-                        "text": [response]
-                    }
-                }
-            ],
-            "source": "abc.theneonproject.org",
-            "payload": {
-                "google": {
-                    "expectUserResponse": True,
-                    "richResponse": {
-                        "items": [
-                            {
-                                "simpleResponse": {
-                                    "textToSpeech": question['text']
-                                }
-                            }
-                        ]
-                    }
-                }
-            },
-            "outputContexts": outputContexts
-        }
+        question_service.get_dialogflow_response(
+            response, question, output_contexts)
     )
